98e16b640b1a4bdce84866afb2889cdce4daeee7
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-04-30 16:02:21 +0900

    reactrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -20,9 +22,22 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/Page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/Page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/Page1DetailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/Page1DetailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/Page2">
           <Page2 />
         </Route>
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1です。</h1>
+      <Link to="Page1/Page1DetailA">DetailA</Link>
+      <br />
+      <Link to="Page1/Page1DetailB">DetailB</Link>
     </div>
   );
 };
