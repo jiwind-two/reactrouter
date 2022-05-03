5f68aea855813950f7b96446f2800b4c6cfb2284
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-05-03 23:48:50 +0900

    reactrouter

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+  const history = useHistory();
+  const onClickDetailA = () => history.push("Page1/Page1DetailA");
   return (
     <div>
       <h1>Page1です。</h1>
-      <Link to="Page1/Page1DetailA">DetailA</Link>
+      <Link to={{ pathname: "Page1/Page1DetailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="Page1/Page1DetailB">DetailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
